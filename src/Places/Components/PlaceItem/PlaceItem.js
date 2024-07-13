013da3b97ed99cb26a4c9104815e4edf8231f707
Eve Bouchard <eve.bouchard@septeo.com>
2024-07-14 00:21:32 +0200

    avant le route params

--- src/Places/Components/PlaceItem/PlaceItem.js
+++ src/Places/Components/PlaceItem/PlaceItem.js
@@ -1,18 +1,28 @@
 import React from "react";
 import "./PlaceItem.css";
 import Card from "../../../Shared/Components/UIElements/Card";
-import Avatar from "../../../Shared/Components/UIElements/Avatar";
 
 const PlaceItem = (props) => {
   return (
     <li className='place-item'>
-      <Card>
-        <Avatar
-          image={props.place.imageUrl}
-          alt='photo illustration'></Avatar>
-        <h1>{props.place.title}</h1>
-        <h2>{props.place.adress}</h2>
-        <h3>{props.place.description}</h3>
+      <Card className='place-item__content'>
+        <div className='place-item__image'>
+          <img
+            src={props.place.imageUrl}
+            alt='illustration'></img>
+        </div>
+
+        <div className='place-item__info'>
+          <h2>{props.place.title}</h2>
+          <h3>{props.place.adress}</h3>
+          <p>{props.place.description}</p>
+        </div>
+
+        <div className='place-item__actions'>
+          <button>VIEW ON MAP</button>
+          <button>EDIT</button>
+          <button>DELETE</button>
+        </div>
       </Card>
     </li>
   );
